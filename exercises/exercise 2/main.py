c63173ae80a13c15150ed0885bcb77b941220ed3
ibrahim_hussiy <ibrahim_hussiny@hotmail.com>
2017-11-29 16:40:24 +0100

    added radail distortion solving

--- exercises/exercise 2/main.py
+++ exercises/exercise 2/main.py
@@ -1,6 +1,8 @@
 import os, sys
 import numpy as np
+from numpy.linalg import inv
 import cv2 as cv
+import cmath
 
 import scipy.io as io
 
@@ -25,8 +27,28 @@ def getIntrinsicParametersMat(K):
 	#Intrinsic matrix = K[I3 | 03]
 	return np.dot(np.array(K.reshape(3,3)),np.append(np.identity(3),np.zeros((3,1)), axis =1))
 
+def correctRadialDistortion(pixelPoints, K, dist_params):
+	results = []
+	for point in pixelPoints:
+		#transform to normalized image coordinates
+		normalizedPoint = np.dot(inv(np.array(K.reshape(3,3))) , np.array([point[0] , point[1] , 1]))
+		# remove last 1 element
+		normalizedPoint = np.delete(normalizedPoint, 2)
+
+		r = cmath.sqrt(pow(normalizedPoint[0],2) + pow(normalizedPoint[1],2) )
+		r= r.real
+		distortionNumber = 1 + dist_params[0] * pow(r,2) + dist_params[1] * pow(r,4) + dist_params[4]* pow(r,6)		
+		
+		undistortedPoint = [x * distortionNumber for x in normalizedPoint]
+		#convert back to pixel coordinates
+		undistortedPoint = np.dot(np.array(K.reshape(3,3)), np.array([undistortedPoint[0],undistortedPoint[1],  1]))
+		#remove last 1 homogenous element, and add to output list
+		results.append([undistortedPoint[0], undistortedPoint[1]])
+	
+	return results
+
 # Project points from 3d world coordinates to 2d image coordinates
-def project_points(X, K, R, T, distortion_flag=False, distortion_params=None):
+def project_points(X, K, R, T):
 	# 2D points in pixel coordinates mapped from the given 3D points 
 	cameraPoints = []
 
@@ -38,15 +60,20 @@ def project_points(X, K, R, T, distortion_flag=False, distortion_params=None):
 		#corresponding point in pixel cooridnates 
 		pixelPoint = np.dot(P, worldPoint)
 		pixelPoint = np.transpose(pixelPoint)
-		#recovering homogeneous coordinates by deviding over the last element, and add it to results
+		#recovering homogeneous coordinates by dividing over the last element, and add it to results
 		cameraPoints.append(pixelPoint[:-1]/pixelPoint[-1])
 
 	return cameraPoints
+
 def project_and_draw(img, X_3d, K, R, T, image_name, distortion_flag, distortion_parameters):
 	
-	projectedPoints = project_points(X_3d, K, R,T,distortion_flag,distortion_parameters)
+	projectedPoints = project_points(X_3d, K, R,T)
+	if distortion_flag == True:
+		 undistortedPoints = correctRadialDistortion(projectedPoints, K, distortion_parameters)
+		 for undistortedPoint in undistortedPoints:
+			 cv.circle(img, tuple(undistortedPoint), 1, (0,255,0), -1)
 	for projectedPoint in projectedPoints:
-		cv.circle(img, tuple(projectedPoint), 1, (0,255,0),-1)
+		cv.circle(img, tuple(projectedPoint), 1, (0,0,255),-1)
 	cv.imwrite(outputFolderPath+image_name, img)
 
 if __name__ == '__main__':
@@ -62,4 +89,4 @@ if __name__ == '__main__':
 	imgs = [cv.imread(base_folder+str(i).zfill(5)+'.jpg') for i in range(TVecs.shape[0])]
 
 	for imageIndex in range(0,TVecs.shape[0]):
-		project_and_draw(imgs[imageIndex], X_3D, Kintr, RMats[imageIndex], TVecs[imageIndex], str(imageIndex).zfill(5)+'.jpg' ,False, kc)
+		project_and_draw(imgs[imageIndex], X_3D, Kintr, RMats[imageIndex], TVecs[imageIndex], str(imageIndex).zfill(5)+'.jpg' ,True, kc)
